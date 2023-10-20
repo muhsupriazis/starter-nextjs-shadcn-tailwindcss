b90dd3744f41a197da0e1fcaa2e771684ed907a8
muhsupriazis <muhsupriazis@gmail.com>
2023-10-20 16:51:21 +0700

    feat: add shadc ui components

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex justify-center items-center min-h-screen">
-      <h1 className="text-center">Aplication under <span className="font-bold underline">development</span></h1>
-    </main>
-  )
-}
